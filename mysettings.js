004fd25a4fd8027934a9d6e8ac0227a4c7827ab2
samaybar <saybar@gmail.com>
2018-02-02 11:23:55 -0500

    added sample settings file

---
+++ mysettings.js
@@ -0,0 +1,11 @@
+//rename me settings.js
+var settings = {
+	username : 'HELPSCOUTAPIUSERNAME',
+	password : 'x',
+	pagesToGet : 3,
+	mailboxId : 1234,//helpscout mailbox id
+	outputJson: false,
+	outputCsv: true
+}
+
+module.exports = settings;
